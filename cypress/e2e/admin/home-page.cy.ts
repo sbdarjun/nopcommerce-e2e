e3d7b31abcb5f6d64e2601b1e56d6a4a8dc53d85
sbdarjun <arjun.eversoft@gmail.com>
2023-04-07 19:29:22 +0545

    added test script for admin home page and hooks implementation

---
+++ cypress/e2e/admin/home-page.cy.ts
@@ -0,0 +1,30 @@
+/// <reference types="Cypress" />
+
+import HomePage from "../../support/page-objects/admin/home-page.po";
+
+describe("Visit admin dashboard", () => {
+  var homePage = new HomePage();
+  beforeEach(() => {
+    cy.login();
+  });
+  it("and validate component", () => {
+    // homePage.visit();
+    homePage.nopCommerceLogo();
+    homePage.sideMenu();
+    homePage.hamburgerIcon();
+    homePage.adminName();
+    homePage.headerContent();
+    homePage.newsBox();
+    homePage.commonStatistics();
+    homePage.orderCard();
+    homePage.newCustomerCard();
+    homePage.totalOrderCard();
+    homePage.orderIncompleteCard();
+    homePage.latestOrderCard();
+    homePage.popularSearchCard();
+    homePage.bestSellerByQtyCard();
+    homePage.bestSellerByAmountCard();
+    homePage.logout().click();
+    cy.visit("/login");
+  });
+});
