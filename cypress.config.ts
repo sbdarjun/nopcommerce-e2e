2a69328f525bcffbd70f03b744f11f55fb306de9
sbdarjun <arjun.eversoft@gmail.com>
2023-05-08 16:14:32 +0545

    mochawesome reporter implementation

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,13 @@ module.exports = {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "reports",
+      overwrite: false,
+      html: true,
+      json: true,
+    },
     baseUrl: "https://www.nopcommerce.com/en",
     adminUrl: "https://admin-demo.nopcommerce.com/",
   },
